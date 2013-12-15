065c0ab6f5d704cb64fbda93dbb0d6504760f049
Chip <cdebergh@gmail.com>
2013-12-15 12:47:28 -0800

    add lat long

--- src/main/java/pizzarev/data/Location.java
+++ src/main/java/pizzarev/data/Location.java
@@ -7,12 +7,32 @@ import javax.persistence.Id;
 public class Location {
 	@Id
 	private Long id;
+	private Double latitude;
+	private Double longitude;
 
+	public Location() { }
+	
 	public Long getId() {
 		return id;
 	}
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public Double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+
+	public Double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
 	}
 }
