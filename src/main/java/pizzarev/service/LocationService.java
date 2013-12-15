43167325f65c22ab01747db6b82ef8e783bbe1ca
Chip <cdebergh@gmail.com>
2013-12-15 15:06:11 -0800

    added put

--- src/main/java/pizzarev/service/LocationService.java
+++ src/main/java/pizzarev/service/LocationService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import pizzarev.data.Location;
@@ -20,5 +22,12 @@ public class LocationService {
 	@ResponseBody
 	public List<Location> getAllLocations() {
 		return locationDao.getAllLocations();
+	}
+	
+	@RequestMapping(value = "/location", method = RequestMethod.PUT)
+	public void put(
+			@RequestBody
+			Location location) {
+		locationDao.save(location);
 	}
 }
