b6a31490d22551b733a99fb4219dea2cd71cbf2a
Chip <cdebergh@gmail.com>
2013-12-15 15:04:25 -0800

    added jpa

--- src/main/java/pizzarev/Application.java
+++ src/main/java/pizzarev/Application.java
@@ -3,12 +3,14 @@ package pizzarev;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @ComponentScan
 @EnableAutoConfiguration
+@EnableJpaRepositories
 public class Application {
 
-  public static void main(String[] args) {
-	  SpringApplication.run(Application.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
