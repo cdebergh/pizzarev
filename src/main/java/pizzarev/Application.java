206fbe670812356e0db6923bb3beb2068989f9e2
Chip <cdebergh@gmail.com>
2013-12-15 12:47:52 -0800

    init

---
+++ src/main/java/pizzarev/Application.java
@@ -0,0 +1,15 @@
+package pizzarev;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.*;
+
+@Configuration
+@ComponentScan
+public class Application {
+
+  public static void main(String[] args) {
+      ApplicationContext context = 
+          new AnnotationConfigApplicationContext(Application.class);
+
+  }
+}
