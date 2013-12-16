8095003f83ce6d06a2e79f13d2df285ea39c8258
Chip <cdebergh@gmail.com>
2013-12-15 17:30:44 -0800

    fixed repository

--- src/main/java/pizzarev/data/LocationDao.java
+++ src/main/java/pizzarev/data/LocationDao.java
@@ -1,66 +1,28 @@
 package pizzarev.data;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.Database;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.PlatformTransactionManager;
 
 @Service
 public class LocationDao {
 	@Autowired
-	private LocationRepository locationRepository;
+	private LocationRepository repository;
 	
-	@Bean
-	public DataSource dataSource() {
-		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
-	}
-
-	@Bean
-	public LocalContainerEntityManagerFactoryBean entityManagerFactory(
-			DataSource dataSource,
-			JpaVendorAdapter jpaVendorAdapter ) {
-		LocalContainerEntityManagerFactoryBean lef = new LocalContainerEntityManagerFactoryBean();
-		lef.setDataSource(dataSource);
-		lef.setJpaVendorAdapter(jpaVendorAdapter);
-		lef.setPackagesToScan("pizzarev");
-		return lef;
-	}
-
-	@Bean
-	public JpaVendorAdapter jpaVendorAdapter() {
-		HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
-		hibernateJpaVendorAdapter.setShowSql(false);
-		hibernateJpaVendorAdapter.setGenerateDdl(true);
-		hibernateJpaVendorAdapter.setDatabase(Database.H2);
-		return hibernateJpaVendorAdapter;
-	}
-
-	@Bean
-	public PlatformTransactionManager transactionManager() {
-		return new JpaTransactionManager();
-	}
-
 	public List<Location> getAllLocations() {
 		List<Location> locations = new ArrayList<Location>();
-		for(Location location : locationRepository.findAll() ) {
+		for(Location location : repository.findAll() ) {
 			locations.add(location);
 		}
 		return locations;
 	}
-	
-	public void save(Location location) {
-		locationRepository.save(location);
+
+	public Location save(Location location) {
+		if(location.getCreatedTimestamp() == null)
+			location.setCreatedTimestamp( new Date() );
+		return repository.save(location);
 	}
 }
